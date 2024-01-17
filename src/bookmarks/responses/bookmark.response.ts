9c4db2dcffe3df7230b9e3ae004bba8c1e3559ce
seungo ha <seungo.ha@actionpower.kr>
2024-01-17 09:35:59 +0900

    fix: trade post list error

--- src/bookmarks/responses/bookmark.response.ts
+++ src/bookmarks/responses/bookmark.response.ts
@@ -12,7 +12,7 @@ export class BookmarkResponse implements IBookmark {
   constructor(partial: Partial<BookmarkResponse>) {
     Object.assign(this, {
       userId: partial?.userId,
-      tradePostIds: partial?.tradePosts,
+      tradePostIds: partial?.tradePostIds,
     });
   }
 }
